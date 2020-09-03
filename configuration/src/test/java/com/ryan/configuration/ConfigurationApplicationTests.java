b647cc68ed2038a4dbc546b10ec10dfbd89eb8b7
富沂 <1312170600@qq.com>
2020-09-03 11:00:44 +0800

    工程初始化

---
+++ configuration/src/test/java/com/ryan/configuration/ConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryan.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
